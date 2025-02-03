032f18a6c351019fc3373ba9371041bde5dc3167
vvt-03 <vaishnavi.vt36@gmail.com>
2025-02-03 15:25:50 +0530

    first commit

---
+++ Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Peer-Mentor Learning Series Workshop 1</h1>
+        <div className="user-info">
+          <p><strong>Name:</strong> Your Name</p> 
+          <p><strong>USN:</strong> Your USN</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
