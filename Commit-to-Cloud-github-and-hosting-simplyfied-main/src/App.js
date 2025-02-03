d5d886f9554c9271cccc82dc40a2babbf9c4d503
vvt-03 <155652755+vvt-03@users.noreply.github.com>
2025-02-03 17:14:33 +0530

    Update App.js

--- Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
+++ Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Your Name</p> 
-          <p><strong>USN:</strong> Your USN</p>
+          <p><strong>Name:</strong> Vaishnavi V T</p> 
+          <p><strong>USN:</strong> 1RVU23CSE523</p>
         </div>
       </header>
     </div>
